5697e4a8b0b29f00a5562c9def83bd5b9342f037
JEZA <jesus.zuniga077@gmail.com>
2022-01-24 13:39:13 -0600

    Agregue BigCalendar de react y estilos personal

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'; //Bigcalendar usa moment
+import moment from 'moment';
+
 import { NavBar } from '../ui/NavBar';
+import { messages } from '../../helper/calendar-messages-es';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es'; //Importar idioma español de moment
+
+moment.locale('es'); //Idioma a español de los dias y meses en calendario por moment
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add( 2, 'hours' ).toDate(),
+    bgcolor: '#fafafa'
+}]
+
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+
+    };
+
     return (
-        <div>
+        <div className="calendar-screen">
             <NavBar />
 
-
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+            />
         </div>
     )
 }
