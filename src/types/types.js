9132269ffbda8705b4d83bf328a8e8cbe67e8578
JEZA <jesus.zuniga077@gmail.com>
2022-03-05 19:34:26 -0600

    authReducer creado

--- src/types/types.js
+++ src/types/types.js
@@ -10,4 +10,12 @@ export const types = {
     eventUpdated: '[event] Event updated',
     eventDeleted: '[event] Event deleted',
 
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
 }
