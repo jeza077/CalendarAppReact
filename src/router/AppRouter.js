76d2c4b79b0f4fce4f93ffdd4202a81d3866b557
JEZA <jesus.zuniga077@gmail.com>
2022-03-13 15:56:20 -0600

    Proteccion de rutas con Router-Dom v6

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     BrowserRouter,
     Routes,
@@ -10,24 +10,44 @@ import { startChecking } from '../actions/auth';
 
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
-  // REVISAR SI ESTA AUTENTICADO
   const dispatch = useDispatch();
+  const { checking, uid } = useSelector( state => state.auth );
 
+  
+  // REVISAR SI ESTA AUTENTICADO
   useEffect(() => {
 
     dispatch( startChecking() );
-
+    
   }, [dispatch])
   
+  if( checking ){
+    return <h5>Espere...</h5>
+  }
 
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/login" element={ <LoginScreen /> } />
-            <Route path="/" element={ <CalendarScreen /> } />
+
+            <Route path="/login" element={ 
+                <PublicRoute uid={ uid }>
+                  <LoginScreen />
+                </PublicRoute>
+              } 
+             />
+
+            <Route path='/*' 
+              element={ 
+                <PrivateRoute uid={ uid }>
+                  <CalendarScreen />
+                </PrivateRoute> 
+                }  
+            />
 
             <Route path="/*" element={ <Navigate to="/" /> } /> {/* Redireccionar al Home si no existe la url */}
         </Routes>
