cf2974e3e62f0e4881b1cc9f0cd4b4168750f60a
JEZA <jesus.zuniga077@gmail.com>
2022-04-10 17:29:55 -0600

    Mostrar color distinto eventos que no son del usuario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -25,6 +25,7 @@ export const CalendarScreen = () => {
 
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar ); //Mostrar eventos en calendario
+    const { uid } = useSelector( state => state.auth );
 
     const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' );
 
@@ -53,9 +54,9 @@ export const CalendarScreen = () => {
     }
     
     const eventStyleGetter = ( event, start, end, isSelected ) => {
-        
+
         const style = {
-            backgroundColor: '#367CF7',
+            backgroundColor: ( uid === event.user._id ) ? '#367CF7' : '#465660',
             borderRadius: '0px',
             opacity: 0.8,
             display: 'block',
