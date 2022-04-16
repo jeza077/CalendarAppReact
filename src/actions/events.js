59832166f07b57b6b8f835d005a6fec5cca5750c
JEZA <jesus.zuniga077@gmail.com>
2022-04-16 11:27:39 -0600

    Eliminar eventos

--- src/actions/events.js
+++ src/actions/events.js
@@ -20,7 +20,7 @@ export const eventStartAddNew = (event) => {
                     _id: uid,
                     name: name
                 }
-                console.log( event );
+                // console.log( event );
                 dispatch( eventAddNew(event) );
             }
 
@@ -68,7 +68,29 @@ const eventUpdated = ( event ) => ({
     payload: event
 });
 
-export const eventDeleted = () => ({ type: types.eventDeleted });
+export const eventStartDelete = () => {
+    return async( dispatch, getState ) => {
+
+        const { id } = getState().calendar.activeEvent;
+
+        try {
+            // console.log(event);
+            const resp = await fetchConToken(`events/${id}`, {}, 'DELETE');
+            const body = await resp.json();
+
+            if(body.ok){
+                dispatch(eventDeleted());
+            } else {
+                Swal.fire('Error', body.msg, 'error');
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const eventDeleted = () => ({ type: types.eventDeleted });
 
 export const eventStartLoading = () => {
     return async( dispatch ) => {
